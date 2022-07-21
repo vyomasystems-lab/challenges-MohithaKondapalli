781fe1c0057e1693beb8ee889f66975fd7f5c471
MohithaKondapalli <kmohitha99@gmail.com>
2022-07-21 16:42:05 +0000

    updated README file

--- level1_design1/README.md
+++ level1_design1/README.md
@@ -1,1 +1,126 @@
-MUX DESIGN VERIFICATION
+# MUX Design Verification
+
+The verification environment is setup using [Vyoma's UpTickPro](https://vyomasystems.com) provided for the hackathon.
+
+gitpod screenshot.PNG
+
+## Verification Environment
+
+The [CoCoTb](https://www.cocotb.org/) based Python test is developed as explained. The test drives random inputs to the Design Under Test (mux) which takes in 2-bit random inputs for each of 31 inputs and 5-bit random input for *sel* and gives 2-bit output *out* based on the sel input
+
+The values are assigned to the input port using 
+```
+dut.sel.value = random.ranint(0,31)
+dut.inp0.value = random.randint(1,3) 
+dut.inp1.value = random.ranint(1,3)
+.
+.
+.
+dut.inp31.value= random.ranint(1,3)
+```
+image.png
+
+The assert statement is used for comparing the mux output to the expected value.
+
+
+
+The following errors are seen:
+```
+assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
+                     AssertionError: the expected output for input sel line 01100 is 01 but the design value is 00
+                    
+ assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
+                     AssertionError: the expected output for input sel line 01101 is 10 but the design value is 11
+
+ assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
+                     AssertionError: the expected output for input sel line 11110 is 01 but the design value is 00
+
+
+```
+## Test Scenario **(Important)**
+- Test Inputs: sel = 12, inp12 = 2
+- Expected Output: out = 2
+- Observed Output in the DUT dut.out=0
+image.png
+## Test Scenario
+- Test Inputs: sel = 13, inp30 = 3
+- Expected Output: out = 3
+- Observed Output in the DUT dut.out=2
+This senario apperas as the case 5'b01101 is written twice 
+image.png
+## Test Scenario
+- Test Inputs: sel = 30, inp30 = 3
+- Expected Output: out = 3
+- Observed Output in the DUT dut.out=0
+image.png
+
+
+Output mismatches for the above inputs proving that there are two design bugs
+
+## Design Bugs
+Based on the above test input and analysing the design, we see the following
+
+```
+5'b01010: out = inp10;
+5'b01011: out = inp11;
+5'b01101: out = inp12;        ====>BUG1
+5'b01101: out = inp13;
+5'b01110: out = inp14;
+```
+In the  design, the case statement for both inp12 and inp13 is same as 5'b01101  it should be ``5'b01100 : out = inp12`` instead of ``5'b01101: out = inp12`` as in the design code. 
+
+```
+5'b11011: out = inp27;
+5'b11100: out = inp28;
+5'b11101: out = inp29;
+default: out = 0;
+```
+In the  design, the case statement for sel input 11110 is not defined. hence it is giving the default output value.  it should be defined as ``5'b11110 : out = inp30`` . 
+
+
+## Design Fix
+Updating the design and re-running the test makes the test pass.
+
+Updated design
+ case(sel)
+      5'b00000: out = inp0;  
+      5'b00001: out = inp1;  
+      5'b00010: out = inp2;  
+      5'b00011: out = inp3;  
+      5'b00100: out = inp4;  
+      5'b00101: out = inp5;  
+      5'b00110: out = inp6;  
+      5'b00111: out = inp7;  
+      5'b01000: out = inp8;  
+      5'b01001: out = inp9;  
+      5'b01010: out = inp10;
+      5'b01011: out = inp11;
+      5'b01100: out = inp12;
+      5'b01101: out = inp13;
+      5'b01110: out = inp14;
+      5'b01111: out = inp15;
+      5'b10000: out = inp16;
+      5'b10001: out = inp17;
+      5'b10010: out = inp18;
+      5'b10011: out = inp19;
+      5'b10100: out = inp20;
+      5'b10101: out = inp21;
+      5'b10110: out = inp22;
+      5'b10111: out = inp23;
+      5'b11000: out = inp24;
+      5'b11001: out = inp25;
+      5'b11010: out = inp26;
+      5'b11011: out = inp27;
+      5'b11100: out = inp28;
+      5'b11101: out = inp29;
+      5'b11110: out = inp30;
+      default: out = 0;
+    endcase
+
+image.png
+
+The updated design is checked in as level1_design1_bugfree/mux.v
+
+## Verification Strategy
+
+## Is the verification complete ?
--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -50,180 +50,180 @@ async def test_mux_buggy(dut):
 
         if sel == 1:
             out = dut.inp1.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp1.value, out, dut.out.value)
             assert out == dut.out.value,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output= dut.out.value)
        
         elif sel == 2:
             out = dut.inp2.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp2.value, out, dut.out.value)
             assert out == dut.out.value,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
 
         elif sel == 3:
             out = dut.inp3.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d  expected output = %d  actual output = %d", sel, dut.inp3.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         
         elif sel == 4:
             out = dut.inp4.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp4.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         
         elif sel == 5:
             out = dut.inp5.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp5.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
 
         elif sel == 6:
             out = dut.inp6.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp6.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
 
         elif sel == 7:
             out = dut.inp7.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp7.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         elif sel == 8:
             out = dut.inp8.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d  input = %d expected output = %d  actual output = %d", sel, dut.inp8.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         
         elif sel == 9:
             out = dut.inp9.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp9.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
 
         elif sel == 10:
             out = dut.inp10.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp10.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
             
         elif sel == 11:
             out = dut.inp11.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp11.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
 
         elif sel == 12:
             out = dut.inp12.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d  input = %d  expected output = %d  actual output = %d", sel, dut.inp12.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
 
         elif sel == 13:
             out = dut.inp13.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d  expected output = %d  actual output = %d", sel, dut.inp13.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
             
         elif sel == 14:
             out = dut.inp14.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp14.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
             
         elif sel == 15:
             out = dut.inp15.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp15.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         
         elif sel == 16:
             out = dut.inp16.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp16.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
 
         elif sel == 17:
             out = dut.inp17.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp17.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
 
         elif sel == 18:
             out = dut.inp18.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp18.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         elif sel == 19:
             out = dut.inp19.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp19.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
 
         elif sel == 20:
             out = dut.inp20.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp20.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         elif sel == 21:
             out = dut.inp21.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp21.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         elif sel == 22:
             out = dut.inp22.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp22.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         elif sel == 23:
             out = dut.inp23.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp23.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         elif sel == 24:
             out = dut.inp24.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp24.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         elif sel == 25:
             out = dut.inp25.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp25.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
 
         elif sel == 26:
             out = dut.inp26.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp26.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         elif sel == 27:
             out = dut.inp27.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp27.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         elif sel == 28:
             out = dut.inp28.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp28.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         elif sel == 29:
             out = dut.inp29.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp29.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         elif sel == 30:
             out = dut.inp30.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp30.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         elif sel == 0:
             out = dut.inp0.value
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d input = %d expected output = %d  actual output = %d", sel, dut.inp31.value, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
         else :
             out = 0
-            dut._log.info("sel = %d , expected output = %d  actual output = %d", sel, out, dut.out.value)
+            dut._log.info("sel = %d  expected output = %d  actual output = %d", sel, out, dut.out.value)
             assert out == dut.out.value ,"the expected output for input sel line {sel} is {expected} but the design value is {output}".format(
                 sel=dut.sel.value, expected = out, output=dut.out.value)
    
