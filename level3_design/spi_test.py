60f7e26671edcfaa6ddf34e1f5caa085f0701dc1
MohithaKondapalli <92357357+MohithaKondapalli@users.noreply.github.com>
2022-08-01 23:00:38 +0530

    updated

--- level3_design/spi_test.py
+++ level3_design/spi_test.py
@@ -44,18 +44,18 @@ async def test(dut):
     dut.RW.value=0b11
     await Timer(1)
     cocotb.log.info(f'clk = {dut.clk.value},cs1bar = {dut.CS1bar.value}')
-    for i in range(9):
-        await FallingEdge(dut.clk)
-        await Timer(3, units= 'ns')
-        cocotb.log.info(f'RX_byte= {dut.MSTR.RX_byte.value}, RX_temp_byte = {dut.MSTR.RX_temp_byte.value} rx_done in master = {dut.MSTR.RX_done.value}, MISO = {dut.MSTR.MISO.value}, out = {dut.MSTR.data_out.value}')
-        
-    for i in range(19):
-        await FallingEdge(dut.clk)
-        await Timer(3, units= 'ns')
-        cocotb.log.info(f'r_data = {dut.SLV_1.R_data.value}, MOSI = {dut.SLV_1.MOSI.value}, out = {dut.SLV_1.data_out.value}')
+    for i in range (34):
+        await RisingEdge(dut.clk)
+    await Timer(1, units='ns')
+    cocotb.log.info(f'RX_byte= {dut.MSTR.RX_byte.value}, RX_temp_byte = {dut.MSTR.RX_temp_byte.value} rx_done in master = {dut.MSTR.RX_done.value}, MISO = {dut.MISO1.value}, out = {dut.data_out_from_master.value}')
+   
+    cocotb.log.info(f'RX_byte= {dut.MSTR.RX_byte.value}, RX_temp_byte = {dut.MSTR.RX_temp_byte.value} rx_done in master = {dut.MSTR.RX_done.value}, MISO = {dut.MISO1.value}, out = {dut.MSTR.data_out.value}')
+    await Timer(1, units='ns')
+    cocotb.log.info(f'r_data = {dut.SLV_1.R_data.value}, MOSI = {dut.MOSI.value}, out = {dut.data_out_slave1.value}')
+   
     
-
-    #cocotb.log.info(f'clk = {dut.clk.value},exact output data from master={(dut.data_out_from_master.value)} and exact output data from slave1 ={(dut.SLV_1.data_out.value)}');
+  
+    cocotb.log.info(f'clk = {dut.clk.value},exact output data from master={(dut.data_out_from_master.value)} and exact output data from slave1 ={(dut.data_out_slave1.value)}');
     
     if((dut.data_out_from_master.value == input_vector1) and (dut.data_out_slave1.value == input_vector)):
         cocotb.log.info(" testcase #1  passed successfuly")
@@ -63,7 +63,7 @@ async def test(dut):
     else:
         cocotb.log.info("testcase #1 failed")
     
-    #cocotb.fork(testcase1(dut, dut.clk, input_vector, input_vector1, input_vector1, input_vector))
+
     dut._log.info("dut.data_into_master = %d data_in_slave1 = %d, expected master output = %d, actualdata_out_from_master = %d, expected_slave1_output = %d, dut.data_out_slave1= %d", dut.data_in_to_master.value, dut.data_in_slave1.value, input_vector1, dut.data_out_from_master.value, input_vector, dut.data_out_slave1.value) 
     assert passed_testcases==1,"FAILED"   	
-  
+   
